ead85b4ef3ba1cd91ed89fc88770c65c5d4dd911
gustavomesc <gustavomesquitacavalcante@gmail.com>
2025-11-11 12:23:39 +0000

    .

--- lista_14/templates/AdminSenha.py
+++ lista_14/templates/AdminSenha.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from viwe import View
+import time
 class AdminSenhaUI:
     def main():
         st.header("alterar Senha do Administrador")
         senha = st.text_input("Informe a nova senha", type="password")
         if st.button("Alterar Senha"):
-            View.admin_alterar_senha(senha)
-            st.success("Senha alterada com sucesso")
+            try:
+                View.admin_alterar_senha(senha)
+                st.success("Senha alterada com sucesso")
+            except ValueError as erro:
+                st.error(erro)
+            time.sleep(2)
+            st.rerun()
+            
